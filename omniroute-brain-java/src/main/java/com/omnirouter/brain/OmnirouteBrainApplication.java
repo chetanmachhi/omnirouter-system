408a19a66716e501e600bd04c1afdbf9a5177806
Chetan Machhi <chetanmacchi@frelitenergy.com>
2026-02-20 12:03:59 +0530

    Initial commit: OmniRouter structure and workspace cleanup

---
+++ omniroute-brain-java/src/main/java/com/omnirouter/brain/OmnirouteBrainApplication.java
@@ -0,0 +1,13 @@
+package com.omnirouter.brain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnirouteBrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnirouteBrainApplication.class, args);
+	}
+
+}
---
+++ omniroute-brain-java/src/test/java/com/omnirouter/brain/OmnirouteBrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omnirouter.brain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnirouteBrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
